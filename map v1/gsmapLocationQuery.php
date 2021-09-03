6bfbb2b4209b7fbc636911bb2d54058390c324ea
Jamaan Alzahrani <jalz0001@student.monash.edu>
2021-09-03 10:42:03 +0300

    adding map v2

--- map v1/gsmapLocationQuery.php
+++ map v1/gsmapLocationQuery.php
@@ -1,21 +1,25 @@
 <?php
-$user = "gsdb";
-$password = "A@1q2w3e";
-$database = "greyspots";
+Class DbCon {
+    private $user = "gsdb";
+    private $password = "A@1q2w3e";
+    private $database = "greyspots";
+}
 
-/* Attempt MySQL server connection. Assuming you are running MySQL
+protected function connect() {
+
+    /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    $pdo = new PDO("mysql:host=localhost;dbname=$this->$database", $this->$user, $this->$password);
     // Set the PDO error mode to exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo
 } catch (PDOException $e) {
+
     die("ERROR: Could not connect. " . $e->getMessage());
 }
-$sql = 'SELECT shortname, lat, lng from COVID_VACCINATION_CENTER_DATA ORDER BY shortname';
-foreach ($pdo->query($sql) as $row) {
-    print $row['shortname'] . "\t";
-    print $row['lat'] . "\t";
-    print $row['lng'] . "\n";
+
 }
+
+
 ?>
